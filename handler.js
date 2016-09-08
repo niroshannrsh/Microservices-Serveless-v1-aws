aeaf3161786af61247fa92ca147ad47ed5520b51
nadunindunil <nadun1indunil@gmail.com>
2016-09-08 17:59:26 +0530

    problems with paths resolved

--- handler.js
+++ handler.js
@@ -1,18 +1,21 @@
 var user = require('./lib/users');
 
 module.exports.users = function(event, context) {
-
-  if(event.path === 'users/getUsers' && event.method === 'GET') {
+  console.log(JSON.stringify(event));
+  if(event.path === '/users/getUsers' && event.method === 'GET') {
     user.getUsers(event,context);
 
-  } else if(event.path === 'users/createUser' && event.method === 'POST') {
+  } else if(event.path === '/users/createUser' && event.method === 'POST') {
   	user.getUsers(event,context);
 
-  } else if(event.path === 'users/updateUser' && event.method === 'POST') {
+  } else if(event.path === '/users/updateUser' && event.method === 'POST') {
   	user.getUsers(event,context);
 
-  } else if(event.path === 'users/getUser' && event.method === 'GET'){
+  } else if(event.path === '/users/getUser' && event.method === 'GET'){
   	user.getUser(event,context);
+  }
+  else{
+    context.error("path not found!");
   }
 
 };
