f968aaec44575f663e964931b187215bec8e6443
nadunindunil <nadun1indunil@gmail.com>
2016-09-09 16:12:50 +0530

    A proper structure was added

--- tasksHandler.js
+++ tasksHandler.js
@@ -1,24 +1,23 @@
 var task = require('./lib/tasks');
 
 module.exports.tasks = function(event, context) {
-  console.log(JSON.stringify(event));
-  if(event.path === '/tasks/getAllActiveTask' && event.method === 'GET') {
-    task.getAllActiveTask(event,context);
 
-  } else if(event.path === '/tasks/getUserTask' && event.method === 'POST') {
-  	task.getUserTask(event,context);
+    if (event.path === '/tasks/getAllActiveTask' && event.method === 'GET') {
+        task.getAllActiveTask(event, context);
 
-  } else if(event.path === '/tasks/updateTaskAdmin' && event.method === 'POST') {
-  	task.updateTaskAdmin(event,context);
+    } else if (event.path === '/tasks/getUserTask' && event.method === 'POST') {
+        task.getUserTask(event, context);
 
-  } else if(event.path === '/tasks/createTaskAdmin' && event.method === 'POST'){
-  	task.createTaskAdmin(event,context);
-  }
-  else if(event.path === '/tasks/updateTaskUser' && event.method === 'POST'){
-  	task.updateTaskUser(event,context);
-  }
-  else{
-    context.succeed(event);
-  }
+    } else if (event.path === '/tasks/updateTaskAdmin' && event.method === 'POST') {
+        task.updateTaskAdmin(event, context);
 
+    } else if (event.path === '/tasks/createTaskAdmin' && event.method === 'POST') {
+        task.createTaskAdmin(event, context);
+
+    } else if (event.path === '/tasks/updateTaskUser' && event.method === 'POST') {
+        task.updateTaskUser(event, context);
+        
+    } else {
+        context.succeed("path was not found!");
+    }
 };
--- usersHandler.js
+++ usersHandler.js
@@ -1,21 +1,19 @@
 var user = require('./lib/users');
 
 module.exports.users = function(event, context) {
-  console.log(JSON.stringify(event));
-  if(event.path === '/users/getUsers' && event.method === 'GET') {
-    user.getUsers(event,context);
 
-  } else if(event.path === '/users/createUser' && event.method === 'POST') {
-  	user.createUser(event,context);
+    if (event.path == '/users/getUsers' && event.method == 'GET') {
+        user.getUsers(event, context);
 
-  } else if(event.path === '/users/updateUser' && event.method === 'POST') {
-  	user.updateUser(event,context);
+    } else if (event.path == '/users/createUser' && event.method == 'POST') {
+        user.createUser(event, context);
 
-  } else if(event.path === '/users/getUser' && event.method === 'GET'){
-  	user.getUser(event,context);
-  }
-  else{
-    context.succeed("path not found!");
-  }
+    } else if (event.path == '/users/updateUser' && event.method == 'POST') {
+        user.updateUser(event, context);
 
+    } else if (event.path == '/users/getUser' && event.method == 'POST') {
+        user.getUser(event, context);
+    } else {
+        context.succeed("path was not found!");
+    }
 };
